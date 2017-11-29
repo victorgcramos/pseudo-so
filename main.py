dbffc6ec8e80ae90ddd52151f2ffec511e1128c6
Victor Guedes <victorgcramos@gmail.com>
2017-11-29 18:39:32 -0200

    adicionei função para o escalonamento das filas de Usuário

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import memory_manager as mm
 import io_manager as iom
 import file_manager as fm
 import process_manager as pm
+
+if __name__ == '__main__':
+    main()
--- process_manager.py
+++ process_manager.py
@@ -1,4 +1,54 @@
+from threading import Thread
+
+class Process:
+    tempo_init = 0
+    prioridade = 0
+    tempo_processador = 0
+    blocos_memoria = 0
+    numero_impressora = 0
+    requisicao_scanner = 0
+    requisicao_modem = 0
+    numero_disco = 0
+
 class ProcessManager:
     fila_tempo_real = []
     fila_usuario = []
-    
+    prioridade_1 = []
+    prioridade_2 = []
+    prioridade_3 = []
+    fila_principal = []
+
+    def novo_processo(self, processo):
+        """
+        insere o processo na fila principal
+        """
+        self.fila_principal.insert(0, processo)
+
+    def escalona_processo_geral(self):
+        """
+        Escalona os processos nas filas de usuario ou fila de tempo real
+        """
+        processo_topo = self.fila_principal[len(self.fila_principal) - 1]
+
+        # distribui os processos ao longo das filas de usuario e tempo real
+        if (processo_topo.prioridade == 0 && len(self.fila_tempo_real) < 1000):
+            self.fila_principal.pop()
+            self.fila_tempo_real.insert(0, processo_topo)
+
+        elif (len(self.fila_usuario) < 1000):
+            # alocou para a fila de usuario
+            self.fila_principal.pop()
+            self.fila_usuario.insert(0, processo_topo)
+
+    def escalona_processo_usuario(self):
+        """
+        Escalona os processos de usuario nas filas de prioridades
+        """
+        processo_topo = fila_usuario[len(self.fila_usuario) - 1]
+        # aloca para a fila de prioridades
+        if (proceso_topo.prioridade == 1):
+            self.prioridade_1.insert(0, processo_topo)
+        elif (proceso_topo.prioridade == 2):
+            self.prioridade_2.insert(0, processo_topo)
+        elif (proceso_topo.prioridade == 3):
+            self.prioridade_3.insert(0, processo_topo)
