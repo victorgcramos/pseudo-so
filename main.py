b636592c3ad7d514a6b6daaddd4859f7c3f270d5
Eduardo da Silva <edusousa1996@hotmail.com>
2017-11-30 20:35:49 -0200

    bora la so tu

--- main.py
+++ main.py
@@ -1,7 +1,66 @@
 import memory_manager as mm
-import io_manager as iom
-import file_manager as fm
+# import io_manager as iom
+# import file_manager as fm
 import process_manager as pm
+import operator
+
+
+def main ():
+    with open('processes.txt', 'r') as f:
+        procs = [[int(x) for x in line.split(',')] for line in f]
+        processes = [pm.Process(x).__dict__ for x in procs]
+    # print processes
+    manager = pm.ProcessManager()
+    memory = mm.MemoryManager()
+    manager.fila_principal = list(sorted(processes, key=operator.itemgetter('tempo_init')))
+    t = 0
+    while(True):
+        #Se tiver processo ainda nao processado
+        if(manager.fila_principal):
+            #Escalona processos do tempo de chegada = t
+            while(manager.fila_principal[0]['tempo_init'] == t):
+                manager.escalona_processo_geral()
+            #Escalona processos da fila de usuario
+            while(manager.fila_usuario):
+                manager.escalona_processo_usuario()
+        #Executa tempo real se tiver
+        if(manager.fila_tempo_real):
+            print 'tempo_real'
+            if(manager.em_execucao):
+                if(manager.em_execucao['prioridade'] > 0):
+                    #INTERROMPE
+                    offset = memory.salva(manager.fila_tempo_real[0])
+                    if(offset != None):
+                        manager.em_execucao = manager.fila_tempo_real.pop(0)
+                        manager.em_execucao['offset'] = offset
+                    #EXECUTA TR
+                else:
+                    print p
+                    #CONTINUA EXECUCAO
+            else:
+                offset = memory.salva(manager.fila_tempo_real[0])
+                if(offset != None):
+                    manager.em_execucao = manager.fila_tempo_real.pop(0)
+                    manager.em_execucao['offset'] = offset
+                #EXECUTA TR
+        #Processa execucao de usuario
+        elif(manager.prioridade_1 or manager.prioridade_2 or manager.prioridade_3):
+            if(manager.em_execucao):
+                #CONTINUA excucao
+            else:
+                # nao tem interrupcao
+                if manager.prioridade_1:
+                    offset = memory.salva(manager.prioridade_1[0])
+                    if(offset != None):
+                        manager.em_execucao = manager.prioridade_1.pop(0)
+                        manager.em_execucao['offset'] = offset
+                    pass
+        else:
+            break
+        manager.em_execucao['tempo_processador'] -= 1
+        if manager.em_execucao['tempo_processador'] == 0:
+            memory.mata(manager.em_execucao)
+        t += 1
 
 if __name__ == '__main__':
     main()
--- process_manager.py
+++ process_manager.py
@@ -2,15 +2,17 @@ from threading import Thread
 import operator
 
 class Process:
-    def __init__(self):
-        self.tempo_init = 0
-        self.prioridade = 0
-        self.tempo_processador = 0
-        self.blocos_memoria = 0
-        self.numero_impressora = 0
-        self.requisicao_scanner = 0
-        self.requisicao_modem = 0
-        self.numero_disco = 0
+    def __init__(self, process):
+        self.tempo_init = process[0]
+        self.prioridade = process[1]
+        self.tempo_processador = process[2]
+        self.blocos_memoria = process[3]
+        self.numero_impressora = process[4]
+        self.requisicao_scanner = process[5]
+        self.requisicao_modem = process[6]
+        self.numero_disco = process[7]
+        self.offset = None
+        self.PID = None
 
 class ProcessManager:
     fila_tempo_real = []
@@ -19,30 +21,28 @@ class ProcessManager:
     prioridade_2 = []
     prioridade_3 = []
     fila_principal = []
-
-    def novo_processo(self, processo):
-        """
-        insere o processo na fila principal
-        """
-        self.fila_principal.insert(0, processo)
+    em_execucao = {}
+    ultimoPID = 0
 
     def escalona_processo_geral(self):
         """
         Escalona os processos nas filas de usuario ou fila de tempo real
         """
-        processo_topo = self.fila_principal[len(self.fila_principal) - 1]
+        processo_topo = self.fila_principal[0]
+        processo_topo['PID'] = ultimoPID
+        ultimoPID += 1
 
         # distribui os processos ao longo das filas de usuario e tempo real
-        if (processo_topo['prioridade'] == 0 && len(self.fila_tempo_real) < 1000):
-            self.fila_principal.pop()
-            self.fila_tempo_real.insert(0, processo_topo)
+        if ((processo_topo['prioridade'] == 0) and (len(self.fila_tempo_real) < 1000)):
+            self.fila_principal.pop(0)
+            self.fila_tempo_real.append(processo_topo)
 
         elif (len(self.fila_usuario) < 1000):
             # alocou para a fila de usuario
-            self.fila_principal.pop()
-            self.fila_usuario.insert(0, processo_topo)
+            self.fila_principal.pop(0)
+            self.fila_usuario.append(processo_topo)
         else:
-            # Caso ele não tenha conseguido alocar o processo nas filas
+            # Caso ele nao tenha conseguido alocar o processo nas filas
             return False
         return True
 
@@ -53,15 +53,15 @@ class ProcessManager:
         processo_topo = fila_usuario[len(self.fila_usuario) - 1]
 
         # aloca para a fila de prioridades
-        elif (processo_topo['prioridade'] == 1 && len(self.prioridade_1) < 1000):
-            self.fila_usuario.pop()
-            self.prioridade_1.insert(0, processo_topo)
-        elif (processo_topo['prioridade'] == 2 && len(self.prioridade_2) < 1000):
-            self.fila_usuario.pop()
-            self.prioridade_2.insert(0, processo_topo)
-        elif (processo_topo['prioridade'] == 3 && len(self.prioridade_3) < 1000):
-            self.fila_usuario.pop()
-            self.prioridade_3.insert(0, processo_topo)
+        if (processo_topo['prioridade'] == 1 and len(self.prioridade_1) < 1000):
+            self.fila_usuario.pop(0)
+            self.prioridade_1.append(processo_topo)
+        elif (processo_topo['prioridade'] == 2 and len(self.prioridade_2) < 1000):
+            self.fila_usuario.pop(0)
+            self.prioridade_2.append(processo_topo)
+        elif (processo_topo['prioridade'] == 3 and len(self.prioridade_3) < 1000):
+            self.fila_usuario.pop(0)
+            self.prioridade_3.append(processo_topo)
         else:
             return False
 
